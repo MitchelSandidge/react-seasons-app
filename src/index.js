0a190ad5addd63075c75b127dd1c77da71ec9182
Mitchel Sandidge <masandidge12@gmail.com>
2020-06-22 11:14:44 -0500

    Added conditionals for the differences of state

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import SeasonDisplay from "./SeasonDisplay";
+// import SeasonDisplay from "./SeasonDisplay";
 
 
 
 class App extends React.Component {
-    render() {
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-            (err) => console.log(err)
-        );
+    // Constructor function is always called on app load,
+    // use this to set initial state
+    constructor(props) {
+        super(props);
 
-        return <div>Latitude: </div>
+        // This is the ONLY time we do direct assignment to this.state
+        this.state = { lat: null, errorMessage: '' };
+
+        window.navigator.geolocation.getCurrentPosition(
+            // we called setState !! :)
+            (position) => this.setState({lat: position.coords.latitude}),
+            (err) => this.setState({ errorMessage: err.message })
+        );
+    }
+
+    // React says we have to define render!!
+    render() {
+      if (this.state.errorMessage && !this.state.lat) {
+          return <div>Error: {this.state.errorMessage}</div>
+      }
+
+      if (!this.state.errorMessage && this.state.lat) {
+          return <div>Latitude: {this.state.lat}</div>
+      }
+
+      return <div>Loading...</div>
+
     }
 }
 
