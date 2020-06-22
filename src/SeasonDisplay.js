d1002b0441568e0765123d8e9944ecbc416da765
Mitchel Sandidge <masandidge12@gmail.com>
2020-06-22 12:30:30 -0500

    Coded corresponding display for the current season

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,5 +1,18 @@
 import React from "react";
 
+
+const seasonConfig = {
+    summer: {
+        text: 'Lets hit the beach!',
+        iconName: 'sun'
+    },
+    winter: {
+        text: 'Burr it\'s cold!',
+        iconName: 'snowflake'
+    }
+};
+
+
 const getSeason = (lat, month) => {
     if (month > 2 && month < 9) {
         return lat > 0 ? 'summer' : 'winter';
@@ -11,9 +24,15 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = (props) => {
     const season = getSeason(props.lat, new Date().getMonth());
+    const { text, iconName } = seasonConfig[ season];
 
-    console.log(season);
-    return <div>Season display!</div>
+    return (
+        <div>
+            <i className={`${iconName} icon`} />
+            <h1> {text} </h1>
+            <i className={`${iconName} icon`} />
+        </div>
+    );
 };
 
 export default SeasonDisplay;
